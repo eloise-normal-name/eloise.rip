8a2f28696a32c2e0179b8eb042d7f7daf573fa93
eloise <eloise@eloise.rip>
2025-10-02 16:19:29 -0700

    turn off feeds / analytics

--- publishconf.py
+++ publishconf.py
@@ -5,9 +5,9 @@ from pelicanconf import *  # noqa
 SITEURL = 'https://eloise.rip'
 RELATIVE_URLS = True
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+# FEED_ALL_ATOM = 'feeds/all.atom.xml'
+# CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 DELETE_OUTPUT_DIRECTORY = True
 
 # Production settings overrides
-GOOGLE_ANALYTICS = ''  # Add if needed
+# GOOGLE_ANALYTICS = ''  # Add if needed
